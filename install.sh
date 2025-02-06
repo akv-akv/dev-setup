43e7b4d2355b306f7aaa7526f52633c9cb904238
Kirill Avilenko <drzoob@gmail.com>
2025-02-06 11:01:25 +0400

    add option to install all

--- install.sh
+++ install.sh
@@ -5,22 +5,31 @@ source ./scripts/base.sh
 show_menu() {
   echo "\nMacOS Setup Script"
   echo "0) Exit"
-  echo "1) Adjust macOS Settings"
-  echo "2) XCode Developer Tools"
-  echo "3) Homebrew"
-  echo "4) Install apps using Homebrew"
+  echo "1) Run all commands"
+  echo "2) Adjust macOS Settings"
+  echo "3) XCode Developer Tools"
+  echo "4) Homebrew"
+  echo "5) Install apps using Homebrew"
   echo "Select an option:"
   read -r choice
+}
+
+run_all_commands() {
+  adjust_macos_settings
+  install_xcode_tools
+  install_homebrew
+  install_apps_using_homebrew
 }
 
 while true; do
   show_menu
   case $choice in
-	0) break;;
-	1) adjust_macos_settings;;
-    2) install_xcode_tools;;
-    3) install_homebrew;;
-    4) install_apps_using_homebrew;;
-	*) e_failure "Invalid option. Please try again.";;
+	  0) break;;
+    1) run_all_commands;;
+  	2) adjust_macos_settings;;
+    3) install_xcode_tools;;
+    4) install_homebrew;;
+    5) install_apps_using_homebrew;;
+	  *) e_failure "Invalid option. Please try again.";;
   esac
 done
